5975d1599bd9eae6de794c0869943aa97507e85f
ThanhDV <thanhdovan210184@gmail.com>
2018-12-06 15:19:03 +0700

    change database

--- TweeterClone/settings.py
+++ TweeterClone/settings.py
@@ -78,12 +78,8 @@ WSGI_APPLICATION = 'TweeterClone.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'TweeterClone',
-        'USER': 'root',
-        'PASSWORD': 'Abc@123456',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
